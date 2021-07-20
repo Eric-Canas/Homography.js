a495ac1fc233058417e8145259b8419d18ba0942
EricCanas <Eric.Canas@e-campus.uab.cat>
2021-07-21 00:24:17 +0200

    Performance improvements

--- Homography.js
+++ Homography.js
@@ -1,49 +1,91 @@
-
+const availableTransforms = ['auto', 'piecewiseaffine', 'affine'];
+const dims = 2;
 
 class Homography {
-    constructor(w=400, h=400){
+    constructor(w=400, h=400, transform = 'piecewiseaffine'){
         this.w = w;
         this.h = h;
+        this.firstTransformSelected = transform.toLowerCase();
+        this.transform = transform.toLowerCase();
         this.hiddenCanvas = document.createElement('canvas');
         this.hiddenCanvas.width = w;
         this.hiddenCanvas.height = h;
         this.hiddenCanvas.style.display = 'hidden';
         this.hiddenCanvasContext = this.hiddenCanvas.getContext("2d");
-        this.piecewiseMatrices = null;
+        // Used to avoid new memory allocation
+        this.auxSrcTriangle = new Int16Array(3*dims);
+        this.auxDstTriangle = new Int16Array(3*dims);
 
     }
 
-    setSourcePoints(points, reescale = false){
-        this.srcPoints = reescale? this.reescalePoints(points, this.w, this.h) : points;
-        this.triangles = null;
-        this.build_triangles();
-
+    setSourcePoints(points){
+        //TODO: Currently, it does not accept normalized coordinates
+        // If it is given as a list, transform it to an Int16Array for improving performance.
+        if(!ArrayBuffer.isView(points)) points = new Int16Array(points.flat())
+        this.srcPoints = points;
+        this.checkAndSelectTransform();
+        if (this.transform === 'piecewiseaffine'){
+            this.build_triangles();
+        }
         //this.piecewiseAffineTransform();
         //this.faceMeshMap.onload = this.onLoadMeshMap.bind(this, w, h);
     }
-
+    /*
     reescalePoints(points, w=400, h=400){
         for (let i = 0; i<points.length; i++){
             points[i][0] *= w;
             points[i][1] *= h;
         }
         return points;
+    }*/
+    checkAndSelectTransform(){
+        /**
+         * Verifies that this.srcPoints is in accordance with the selected transform, or select one transform if 'auto' 
+         */
+        if (this.transform === 'auto'){
+            if (this.srcPoints.length === 3*dims) this.transform = 'affine';
+            else if (this.srcPoints.length > 3*dims) this.transform = 'piecewiseaffine';
+            //TODO: Select best transform
+        } else if (this.transform === 'piecewiseaffine'){
+            // If it have only 3 points it is an affine transform.
+            if (this.srcPoints.length === 3*dims){
+                this.transform = 'affine';
+                console.warn('Only 3 source points given but "piecewiseAffine" transform selected. Transform changed to "affine".');
+            } else if (this.srcPoints.length < 3*dims){
+                throw(`A piecewise (or affine) transform needs to determine least three reference points but only ${this.srcPoints.length/dims} were given`);
+            }
+        } else if (this.transform === 'affine'){
+            if (this.srcPoints.length !== 3*dims){
+                throw(`An affine transform needs to determine exactly three reference points but ${this.srcPoints.length/dims} were given`)
+            }
+        }
     }
-
     build_triangles(){
         this.triangles = Delaunay(this.srcPoints);
         this.trianglesCorrespondencesMatrix = this.buildTrianglesCorrespondencesMatrix();
     }
 
-    piecewiseAffineTransform(dstPoints, reescale = false){
-        dstPoints = reescale? this.reescalePoints(dstPoints, this.w, this.h) : dstPoints;
+    setDstPoints(dstPoints){
+        if (this.transform === 'piecewiseaffine') this.piecewiseMatrices = this.piecewiseAffineTransform(dstPoints.flat());
+        
+    }
+
+    piecewiseAffineTransform(dstPoints){
         let piecewiseMatrices = []
         for(const triangle of this.triangles){
-            const srcTriangle = [this.srcPoints[triangle[0]], this.srcPoints[triangle[1]], this.srcPoints[triangle[2]]];
-            const dstTriangle = [dstPoints[triangle[0]], dstPoints[triangle[1]], dstPoints[triangle[2]]];
-            piecewiseMatrices.push(TransformationMatrix.fromTriangles(srcTriangle, dstTriangle))
+            // Set in the already allocated memory for doing it faster and keep it as an Int16Array (It would be nice to check other options (including async function))
+            //Set the srcTriangle
+            this.auxSrcTriangle[0] = this.srcPoints[triangle[0]*dims]; this.auxSrcTriangle[1] = this.srcPoints[triangle[0]*dims+1];
+            this.auxSrcTriangle[2] = this.srcPoints[triangle[1]*dims]; this.auxSrcTriangle[3] = this.srcPoints[triangle[1]*dims+1];
+            this.auxSrcTriangle[4] = this.srcPoints[triangle[2]*dims]; this.auxSrcTriangle[5] = this.srcPoints[triangle[2]*dims+1];
+            //Set the dstTriangle
+            this.auxDstTriangle[0] = dstPoints[triangle[0]*dims]; this.auxDstTriangle[1] = dstPoints[triangle[0]*dims+1];
+            this.auxDstTriangle[2] = dstPoints[triangle[1]*dims]; this.auxDstTriangle[3] = dstPoints[triangle[1]*dims+1];
+            this.auxDstTriangle[4] = dstPoints[triangle[2]*dims]; this.auxDstTriangle[5] = dstPoints[triangle[2]*dims+1];
+            
+            piecewiseMatrices.push(affineMatrixFromTriangles(this.auxSrcTriangle, this.auxDstTriangle))
         }
-        this.piecewiseMatrices = piecewiseMatrices;
+        return piecewiseMatrices;
     }
 
     onLoadMeshMap(w, h){
@@ -55,13 +97,12 @@ class Homography {
         const rgba_size = 4;
         image = this.getImageAsRGBAArray(image);
         let output_img = new Uint8ClampedArray(image.length).fill(0); //Change to set as the image after debugging
-        console.log(output_img)
         for (let h = 0; h < this.h; h++){
             for (let w = 0; w < this.w; w++){
                 const inTriangle = this.trianglesCorrespondencesMatrix[h*this.w+w]
                 if (inTriangle > -1){
                     const idx = h*this.w*rgba_size+w*rgba_size;
-                    let [newX, newY] = TransformationMatrix.applyToPoint(this.piecewiseMatrices[inTriangle], [w, h]);
+                    let [newX, newY] = applyTransformToPoint(this.piecewiseMatrices[inTriangle], w, h);//TransformationMatrix.applyToPoint(this.piecewiseMatrices[inTriangle], [w, h]);
                     newX = Math.round(newX); newY = Math.round(newY);
                     const newIdx = newY*this.w*rgba_size+newX*rgba_size;
                     output_img[newIdx] = image[idx], output_img[newIdx+1] = image[idx+1], output_img[newIdx+2] = image[idx+2], output_img[newIdx+3] = 255; 
@@ -71,26 +112,30 @@ class Homography {
         return output_img;
     }
 
-    buildTrianglesCorrespondencesMatrix(method='floodFill'){
+    buildTrianglesCorrespondencesMatrix(method='circumscribed'){
+        // TODO: TIME SPENT IS HEREEE! Solve it;
         this.trianglesCorrespondencesMatrix = new Int16Array(this.w* this.h).fill(-1);
-        //TODO: Solve bug, circumscribed version allows to go to h, w while floodFill must be configured to h-1, w-1
         switch(method){
             case 'floodFill':
                 for (const [i, triangle] of Object.entries(this.triangles)){
-                    const srcTriangle = [this.srcPoints[triangle[0]], this.srcPoints[triangle[1]], this.srcPoints[triangle[2]]];
-                    this.fillByFloodFill(srcTriangle, i)
+                    this.auxSrcTriangle[0] = this.srcPoints[triangle[0]*dims]; this.auxSrcTriangle[1] = this.srcPoints[triangle[0]*dims+1];
+                    this.auxSrcTriangle[2] = this.srcPoints[triangle[1]*dims]; this.auxSrcTriangle[3] = this.srcPoints[triangle[1]*dims+1];
+                    this.auxSrcTriangle[4] = this.srcPoints[triangle[2]*dims]; this.auxSrcTriangle[5] = this.srcPoints[triangle[2]*dims+1];
+                    this.fillByFloodFill(this.auxSrcTriangle, i)
                 }
                 break;
             case 'circumscribed':
                 for (const [i, triangle] of Object.entries(this.triangles)){
-                    const srcTriangle = [this.srcPoints[triangle[0]], this.srcPoints[triangle[1]], this.srcPoints[triangle[2]]];
-                    this.fillByCircumscribedRectangle(srcTriangle, i);
-                    console.log("FILLED")
+                    //Set the srcTriangle
+                    this.auxSrcTriangle[0] = this.srcPoints[triangle[0]*dims]; this.auxSrcTriangle[1] = this.srcPoints[triangle[0]*dims+1];
+                    this.auxSrcTriangle[2] = this.srcPoints[triangle[1]*dims]; this.auxSrcTriangle[3] = this.srcPoints[triangle[1]*dims+1];
+                    this.auxSrcTriangle[4] = this.srcPoints[triangle[2]*dims]; this.auxSrcTriangle[5] = this.srcPoints[triangle[2]*dims+1];
+                    this.fillByCircumscribedRectangle(this.auxSrcTriangle, i);
                 }
                 break;
         }
         
-
+        /*
         let asMatrix = [];
         for(let h = 0; h < this.h; h++){
             let row = [];
@@ -100,8 +145,8 @@ class Homography {
             asMatrix.push(row)
         }
 
-        console.table(asMatrix);
-        
+        console.table(asMatrix);*/
+    
        return this.trianglesCorrespondencesMatrix;
     }
 
@@ -125,7 +170,7 @@ class Homography {
         const rectangle = rectangleCircunscribingTriangle(triangle);
         for (let x = rectangle.x; x < rectangle.width; x++){
             for (let y = rectangle.y; y < rectangle.height; y++){
-                if (pointInTriangle([x, y], triangle)){
+                if (pointInTriangle(x, y, triangle)){
                     this.trianglesCorrespondencesMatrix[y * this.w + x] = idx;
                 }
             }
@@ -133,8 +178,9 @@ class Homography {
     }
 
     fillByFloodFill(triangle, idx){
-        const point = [Math.round(triangle[0][0]), Math.round(triangle[0][1])];
-
+        const point = [Math.round(triangle[0]), Math.round(triangle[1])];
+        if (point[0] == this.w) point[0]-=1;
+        if (point[1] == this.h) point[1]-=1;
         this.trianglesCorrespondencesMatrix[point[0]*this.w+point[1]] = idx;
         // North
         this.floodFill(triangle, [point[0], point[1]-1], idx);
@@ -149,8 +195,7 @@ class Homography {
     floodFill(triangle, point, idx){
         const index = point[0]*this.w+point[1];
         if(this.trianglesCorrespondencesMatrix[index] < 0 && 
-            pointInTriangle(point, triangle)){
-
+            pointInTriangle(point[0], point[1], triangle)){
             this.trianglesCorrespondencesMatrix[index] = idx;
              // North
             this.floodFill(triangle, [point[0], point[1]-1], idx);
@@ -169,7 +214,7 @@ export {Homography}
 
 function Delaunay(points){
     /*Import library from <script src="https://unpkg.com/delaunator@5.0.0/delaunator.min.js"></script>*/
-    const triangles = new Delaunator(points.flat()).triangles;
+    const triangles = new Delaunator(points).triangles;
     let triangles_idx = [];
     for (let i = 0; i < triangles.length; i += 3) {
         triangles_idx.push([triangles[i], triangles[i+1], triangles[i+2]]);
@@ -177,16 +222,66 @@ function Delaunay(points){
     return triangles_idx;
 }
 
+function affineMatrixFromTriangles(srcTriangle, dstTriangle){
+    /**
+     * Gets the 2x3 transform matrix from two triangles got as TypedArray of positions (for performance reasons)
+     */
+        
+        // Set the [[a, b, c], [d, e, f]] points of the matrix but as variables, for avoiding memory allocations until the last moment
+        // Src matrix (that will be inversed later)
+        const srcE = srcTriangle[4];
+        const srcF = srcTriangle[5];
 
-//TODO: Understand and modify this (comes from https://github.com/mattdesl/point-in-triangle)
-//http://www.blackpawn.com/texts/pointinpoly/
-function pointInTriangle(point, triangle) {
+        const srcA = srcTriangle[0]-srcE;
+        const srcB = srcTriangle[1]-srcF;
+        const srcC = srcTriangle[2]-srcE;
+        const srcD = srcTriangle[3]-srcF;
+
+        // Dst matrix (that will be fix)
+        const dstE = dstTriangle[4];
+        const dstF = dstTriangle[5];
+
+        const dstA = dstTriangle[0]-dstE;
+        const dstB = dstTriangle[1]-dstF;
+        const dstC = dstTriangle[2]-dstE;
+        const dstD = dstTriangle[3]-dstF;
+      
+        //Inverse the source matrix
+        const denominator = srcA * srcD - srcB * srcC;
+        
+        const invSrcA = srcD / denominator;
+        const invSrcB = srcB / -denominator;
+        const invSrcC = srcC / -denominator;
+        const invSrcD = srcA / denominator;
+        const invSrcE = (srcD * srcE - srcC * srcF) / -denominator;
+        const invSrcF = (srcB * srcE - srcA * srcF) / denominator;
+
+        // Define the affineMatrix as the matrix multiplication of dstMatrix * srcMatrix'
+        const affineMatrix = new Float32Array([
+            (dstA * invSrcA) + (dstC * invSrcB), // a
+            (dstB * invSrcA) + (dstD * invSrcB), // b
+            (dstA * invSrcC) + (dstC * invSrcD), // c
+            (dstB * invSrcC) + (dstD * invSrcD), // d
+            (dstA * invSrcE) + (dstC * invSrcF) + dstE, //e
+            (dstB * invSrcE) + (dstD * invSrcF) + dstF  //f
+        ]);
+        // Some codes rounds it to a maximum decimal for smoothing reasons
+        return affineMatrix
+
+}
+
+function applyTransformToPoint(matrix, x, y){
+    return [(matrix[0] * x) + (matrix[2] * y) + matrix[4], //x
+            (matrix[1] * x) + (matrix[3] * y) + matrix[5]] //y
+}
+
+//This code is slightly adapted from (https://github.com/mattdesl/point-in-triangle [http://www.blackpawn.com/texts/pointinpoly/])
+function pointInTriangle(x, y, triangle) {
+
     //compute vectors & dot products
-    var cx = point[0], cy = point[1],
-        t0 = triangle[0], t1 = triangle[1], t2 = triangle[2],
-        v0x = t2[0]-t0[0], v0y = t2[1]-t0[1],
-        v1x = t1[0]-t0[0], v1y = t1[1]-t0[1],
-        v2x = cx-t0[0], v2y = cy-t0[1],
+    const v0x = triangle[4]-triangle[0], v0y = triangle[5]-triangle[1],
+        v1x = triangle[2]-triangle[0], v1y = triangle[3]-triangle[1],
+        v2x = x-triangle[0], v2y = y-triangle[1],
         dot00 = v0x*v0x + v0y*v0y,
         dot01 = v0x*v1x + v0y*v1y,
         dot02 = v0x*v2x + v0y*v2y,
@@ -194,17 +289,17 @@ function pointInTriangle(point, triangle) {
         dot12 = v1x*v2x + v1y*v2y
 
     // Compute barycentric coordinates
-    var b = (dot00 * dot11 - dot01 * dot01),
+    const b = (dot00 * dot11 - dot01 * dot01),
         inv = b === 0 ? 0 : (1 / b),
         u = (dot11*dot02 - dot01*dot12) * inv,
         v = (dot00*dot12 - dot01*dot02) * inv
-    return u>=0 && v>=0 && (u+v <= 1)
+    return u>=0 && v>=0 && (u+v < 1)
 }
 
 function rectangleCircunscribingTriangle(triangle){
-    const x = Math.min(triangle[0][0], triangle[1][0], triangle[2][0]);
-    const y = Math.min(triangle[0][1], triangle[1][1], triangle[2][1]);
-    const width = Math.max(triangle[0][0], triangle[1][0], triangle[2][0])-x;
-    const height =  Math.max(triangle[0][1], triangle[1][1], triangle[2][1])-y;
+    const x = Math.min(triangle[0], triangle[2], triangle[4]);
+    const y = Math.min(triangle[1], triangle[3], triangle[5]);
+    const width = Math.max(triangle[0], triangle[2], triangle[4])-x;
+    const height =  Math.max(triangle[1], triangle[3], triangle[5])-y;
     return {x : Math.floor(x), y : Math.floor(y), width : Math.ceil(width), height : Math.ceil(height)};
 }
--- test/test.js
+++ test/test.js
@@ -1,40 +1,139 @@
 import {Homography} from '../Homography.js';
 
-const test = 1;
-
-const w = 20, h = 20;
-// Build the canvas
-const canvasContext = document.getElementById('cnvs').getContext("2d");
+const w = 400, h = 400;
+const padBetweenImgs = w/4;
 // Charge testImg
 let testImg = document.createElement('img');
 testImg.src = './testImg.png'
-testImg.onload = () => runTest(test);
+testImg.onload = () => runTests();
 
 
-function runTest(test){
-    switch (test){
-       case 1 :
-           testOne(); 
-    }
+function runTests(){
+    test1();
+    test2();
+    test3();
+    test4();
+    test5();
+
 }
 
-function testOne(){
+function test1(){
+    const squarePoints = [[0, 0], [0, h], [w, 0], [w, h]];
+    let canvasContext = createCanvasContext("Identity Transform")
     canvasContext.drawImage(testImg, 0, 0, w, h);
     canvasContext.fill();
-
-    const squarePoints = [[0, 0], [0, h-1], [w-1, 0], [w-1, h-1]];
-
+    //const s0 = performance.now();
     const identityHomography = new Homography(w, h);
+    const s0 = performance.now();
     identityHomography.setSourcePoints(squarePoints);
-    identityHomography.piecewiseAffineTransform(squarePoints);
+    const a = performance.now();
+    identityHomography.setDstPoints(squarePoints);
+    const b = performance.now();
     const result = identityHomography.warp(testImg);
+    const c = performance.now();
     const img = identityHomography.getImageFromRGBAArray(result);
-    img.onload = (() => {canvasContext.drawImage(img, w+50, 0, w, h);
-                        canvasContext.fill();
-                        console.log("Printed?");}).bind(this)
-
-
+    const d = performance.now();
+    console.log(`Create and Set source Time ${(a-s0)/1000} s, calculate transformation matrices ${(b-a)/1000} s, warp time ${(c-b)/1000}, transform to img ${(d-c)/1000}`); 
+    const s1 = performance.now();
+    addSecondsToTitle((s1-s0)/1000)
+    img.onload = (() => {canvasContext.drawImage(img, w+padBetweenImgs, 0, w, h);
+                        canvasContext.fill();}).bind(this)
 }
 
+function test2(){
+    const squarePoints = [[0, 0], [0, h], [w, 0], [w, h]];
+    const dstPoints = [[0, 0], [0, h], [w, 0], [w*3/4, h*3/4]];
+    let canvasContext = createCanvasContext("Bottom-Left triangle shortened")
+    canvasContext.drawImage(testImg, 0, 0, w, h);
+    canvasContext.fill();
+    const s0 = performance.now();
+    const identityHomography = new Homography(w, h);
+    identityHomography.setSourcePoints(squarePoints);
+    identityHomography.setDstPoints(dstPoints);
+    const result = identityHomography.warp(testImg);
+    const img = identityHomography.getImageFromRGBAArray(result);
+    const s1 = performance.now();
+    addSecondsToTitle((s1-s0)/1000)
+    img.onload = (() => {canvasContext.drawImage(img, w+padBetweenImgs, 0, w, h);
+                        canvasContext.fill();}).bind(this)
+}
+
+function test3(){
+    const squarePoints = [[0, 0], [0, h], [w, 0], [w, h]];
+    const dstPoints = [[0, 0], [0, h], [w, 0], [w/4, h/4]];
+    let canvasContext = createCanvasContext("Superposition")
+    canvasContext.drawImage(testImg, 0, 0, w, h);
+    canvasContext.fill();
+    const s0 = performance.now();
+    const identityHomography = new Homography(w, h);
+    identityHomography.setSourcePoints(squarePoints);
+    identityHomography.setDstPoints(dstPoints);
+    const result = identityHomography.warp(testImg);
+    const img = identityHomography.getImageFromRGBAArray(result);
+    const s1 = performance.now();
+    addSecondsToTitle((s1-s0)/1000)
+    img.onload = (() => {canvasContext.drawImage(img, w+padBetweenImgs, 0, w, h);
+                        canvasContext.fill();}).bind(this)
+}
+
+function test4(){
+    const squarePoints = [[0, 0], [0, h], [w, 0], [w, h]];
+    const dstPoints = [[w/6, h/6], [0, h/2], [w, 0], [w*7/8, h*3/8]];
+    let canvasContext = createCanvasContext("Both Triangle shortened")
+    canvasContext.drawImage(testImg, 0, 0, w, h);
+    canvasContext.fill();
+    const s0 = performance.now();
+    const identityHomography = new Homography(w, h);
+    identityHomography.setSourcePoints(squarePoints);
+    identityHomography.setDstPoints(dstPoints);
+    const result = identityHomography.warp(testImg);
+    const img = identityHomography.getImageFromRGBAArray(result);
+    const s1 = performance.now();
+    addSecondsToTitle((s1-s0)/1000)
+    img.onload = (() => {canvasContext.drawImage(img, w+padBetweenImgs, 0, w, h);
+                        canvasContext.fill();}).bind(this)
+}
+
+function test5(){
+    let newH  = h/3;
+    const squarePoints = [[0, 0], [0, newH], [w, 0], [w, newH]];
+    const dstPoints = [[0, 0], [0, newH], [w, 0], [w*3/4, newH*3/4]];
+    let canvasContext = createCanvasContext("Rectangular Images (Bottom-Left triangle shortened)")
+    canvasContext.drawImage(testImg, 0, 0, w, newH);
+    canvasContext.fill();
+    const s0 = performance.now();
+    const identityHomography = new Homography(w, newH);
+    identityHomography.setSourcePoints(squarePoints);
+    identityHomography.setDstPoints(dstPoints);
+    const result = identityHomography.warp(testImg);
+    const img = identityHomography.getImageFromRGBAArray(result);
+    const s1 = performance.now();
+    addSecondsToTitle((s1-s0)/1000)
+    img.onload = (() => {canvasContext.drawImage(img, w+padBetweenImgs, 0, w, newH);
+                        canvasContext.fill();}).bind(this)
+}
+
+function createCanvasContext(title = "Test"){
+    let div = document.createElement('div');
+    div.style.width = '80%';
+    let h1 = document.createElement('h1');
+    h1.textContent = title;
+    h1.style.textAlign = 'center';
+    div.appendChild(h1);
+    // Build the canvas
+    let canvas = document.createElement('canvas');
+    canvas.height = h;
+    canvas.width = w*2+padBetweenImgs;
+    canvas.style.width = '100%'
+    canvas.style.display = 'block'
+    div.appendChild(canvas);
+    document.body.appendChild(div);
+    return canvas.getContext("2d");
+}
+
+function addSecondsToTitle(seconds){
+    let lastH1 = document.body.lastChild.firstChild;
+    lastH1.textContent += ` [${seconds.toFixed(3)} s]`
+}
 
 
